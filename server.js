94e4b68d07874c91d2c5b9aa0c7dd68e538712ef
Ross Gardner <gar17014@byui.edu>
2023-10-13 10:30:46 -0600

    Completed Unit 3 Activities

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
+// app.get("/", baseController.buildHome)
 
 /*app.get("/", function(req, res){
   res.render("index", {title: "Home"})
@@ -48,9 +50,10 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message, // : err.message,
     nav
   })
 })
