f79a28ef38ae68e853191b588639336d44a3c9cf
Ross Gardner <gar17014@byui.edu>
2023-10-19 16:29:59 -0600

    Commented a line of code

--- server.js
+++ server.js
@@ -38,6 +38,8 @@ app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute)
 
+// app.get("/500", utilities.handleErrors(baseController.buildHome))
+
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
