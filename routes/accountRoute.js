5543b4e7ce670c01a7e660b3e5141cef9bd2e4fa
Ross Gardner <gar17014@byui.edu>
2023-11-02 16:03:23 -0600

    Worked on email check and form stickiness activity

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -19,4 +19,14 @@ router.post(
     Utilities.handleErrors(accountController.registerAccount)
 )
 
+// Process the login attempt
+router.post(
+    "/login",
+    regValidate.loginRules(),
+    regValidate.checkLogData,
+    (req, res) => {
+      res.status(200).send('login process')
+    }
+)
+
 module.exports = router;
