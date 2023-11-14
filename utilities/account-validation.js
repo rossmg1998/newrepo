856eea27804628db23b3368f851ec10bb294a4aa
Ross Gardner <gar17014@byui.edu>
2023-11-14 12:20:22 -0700

    Fixed custom email check

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -26,10 +26,17 @@ validate.registrationRules = () => {
       .isEmail()
       .normalizeEmail() // refer to validator.js docs
       .withMessage("A valid email is required.")
-      .custom(async (account_email) => {
-        const emailExists = await accountModel.checkExistingEmail(account_email)
-        if (emailExists){
-          throw new Error("Email exists. Please log in or use different email")
+      .custom(async (account_email, {req}) => {
+        const account_id = req.body.account_id
+        const account = await accountModel.getAccountById(account_id)
+        // Check if submitted email is same as existing
+        if (account_email != account.account_email) {
+          // No - Check if email exists in table
+          const emailExists = await accountModel.checkExistingEmail(account_email)
+          // Yes - throw error
+          if (emailExists.count != 0) {
+            throw new Error("Email exists. Please log in or use different email")
+          }
         }
       }),
   
@@ -58,10 +65,17 @@ validate.loginRules = () => {
     .isEmail()
     .normalizeEmail() // refer to validator.js docs
     .withMessage("A valid email is required.")
-    .custom(async (account_email) => {
-      const emailExists = await accountModel.checkExistingEmail(account_email)
-      if (emailExists){
-        throw new Error("Email exists. Please log in or use different email")
+    .custom(async (account_email, {req}) => {
+      const account_id = req.body.account_id
+      const account = await accountModel.getAccountById(account_id)
+      // Check if submitted email is same as existing
+      if (account_email != account.account_email) {
+        // No - Check if email exists in table
+        const emailExists = await accountModel.checkExistingEmail(account_email)
+        // Yes - throw error
+        if (emailExists.count != 0) {
+          throw new Error("Email exists. Please use a different email")
+        }
       }
     }),
 
