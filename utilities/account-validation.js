07e691a85fd4e806547af01f9e4421b5c23974d7
Ross Gardner <gar17014@byui.edu>
2023-12-01 17:43:51 -0700

    Account update validation middleware email rule

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -26,18 +26,18 @@ validate.registrationRules = () => {
         .isEmail()
         .normalizeEmail() // refer to validator.js docs
         .withMessage("A valid email is required.")
-        .custom(async (account_email) => {  // , {req}
-          // const account_id = req.body.account_id
-          // const account = await accountModel.getAccountById(account_id)
+        .custom(async (account_email, {req}) => {  // , {req}
+          const account_id = req.body.account_id
+          const account = await accountModel.getAccountById(account_id)
           // Check if submitted email is same as existing
-          // if (account_email != account.account_email) {
+          if (account_email != account.account_email) {
           // No - Check if email exists in table
           const emailExists = await accountModel.checkExistingEmail(account_email)
           // Yes - throw error
-          if (emailExists) { // .count != 0
+          if (emailExists.count != 0) { // .count != 0
             throw new Error("Email exists. Please use a different email")
           }
-          // }
+          }
         }),
   
       // password is required and must be strong password
